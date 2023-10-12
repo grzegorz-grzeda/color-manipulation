628e73f622520ae9ba4ea8ffc8e36165da542727
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2023-10-12 14:13:42 +0200

    Changed unit test library

--- tests/test-color-manipulation.c
+++ tests/test-color-manipulation.c
@@ -8,8 +8,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -19,16 +19,15 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+#include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <setjmp.h>
-#include <cmocka.h>
+#include "cmocka.h"
 
 #include "color-manipulation.h"
 
-static void test_convert_hsv_to_rgb(void **state)
-{
+static void test_convert_hsv_to_rgb(void** state) {
     color_hsv_t hsv = {.hue = 10, .saturation = 100, .value = 100};
     color_rgb_t rgb = {0};
     rgb = convert_color_hsv_to_rgb(hsv);
@@ -36,41 +35,40 @@ static void test_convert_hsv_to_rgb(void **state)
     assert_int_equal(rgb.green, 0x2A);
     assert_int_equal(rgb.blue, 0x00);
 
-    hsv.hue=77;
-    hsv.saturation=200;
+    hsv.hue = 77;
+    hsv.saturation = 200;
     rgb = convert_color_hsv_to_rgb(hsv);
     assert_int_equal(rgb.red, 0xB7);
     assert_int_equal(rgb.green, 0xFF);
     assert_int_equal(rgb.blue, 0x00);
 
-    hsv.hue=140;
-    hsv.value=190;
+    hsv.hue = 140;
+    hsv.value = 190;
     rgb = convert_color_hsv_to_rgb(hsv);
     assert_int_equal(rgb.red, 0x00);
     assert_int_equal(rgb.green, 0xFF);
     assert_int_equal(rgb.blue, 0x55);
 
-    hsv.hue=205;
+    hsv.hue = 205;
     rgb = convert_color_hsv_to_rgb(hsv);
     assert_int_equal(rgb.red, 0x00);
     assert_int_equal(rgb.green, 0x95);
     assert_int_equal(rgb.blue, 0xFF);
 
-    hsv.hue=259;
+    hsv.hue = 259;
     rgb = convert_color_hsv_to_rgb(hsv);
     assert_int_equal(rgb.red, 0x50);
     assert_int_equal(rgb.green, 0x00);
     assert_int_equal(rgb.blue, 0xFF);
 
-    hsv.hue=312;
+    hsv.hue = 312;
     rgb = convert_color_hsv_to_rgb(hsv);
     assert_int_equal(rgb.red, 0xFF);
     assert_int_equal(rgb.green, 0x00);
     assert_int_equal(rgb.blue, 0xCC);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_convert_hsv_to_rgb),
     };
